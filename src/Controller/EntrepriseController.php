e1203563c98fbaac5610425f296b0f61961b1cfb
Xylo <theo.arbogast@hotmail.fr>
2025-01-21 12:01:26 +0100

    feat/update : créations de méthodes de récupération en BdD + affichage Employe et Entreprise

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -2,21 +2,32 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Entreprise;
+use App\Repository\EntrepriseRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class EntrepriseController extends AbstractController
 {
     #[Route('/entreprise', name: 'app_entreprise')]
-    public function index(): Response
+    // public function index(EntityManagerInterface $entityManager): Response
+    public function index(EntrepriseRepository $entrepriseRepository): Response
     {
-        $name = 'Elan Formation';
-        $tableau = ["valeur 1", "valeur 2", "valeur 3"];
+        // $name = 'Elan Formation';
+        // $tableau = ["valeur 1", "valeur 2", "valeur 3"];
+        // $entreprises = $entityManager->getRepository(Entreprise::class)->findAll();
+        // $entreprises = $entrepriseRepository->findAll();
+
+        // equivalent SQL : SELECT * FROM entreprise WHERE ville = "Strasbourg" ORDER BY raisonSociale (ASC)
+        $entreprises = $entrepriseRepository->findBy(["ville" => "Strasbourg"], ["raisonSociale" => "ASC"]);
+
         return $this->render('entreprise/index.html.twig', [
             'controller_name' => 'EntrepriseController',
-            'name' => $name,
-            'tableau' => $tableau
+            'entreprises' => $entreprises
+            // 'name' => $name,
+            // 'tableau' => $tableau
         ]);
     }
 }
